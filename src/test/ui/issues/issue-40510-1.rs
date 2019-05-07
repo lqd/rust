a8bf2d3e75fe82af78864feadc1a60d0dc91b6cd
lqd <remy.rakic+github@gmail.com>
2019-05-07 17:48:21 +0200

    Ignore polonius compare-mode in 3 tests
    These tests have a lot of combinations which can't be fully encoded in the compare-mode framework: either compile-pass or compile-fail depending on the NLL migrate mode or full NLLs.

--- src/test/ui/issues/issue-40510-1.rs
+++ src/test/ui/issues/issue-40510-1.rs
@@ -1,3 +1,15 @@
+// This test is validating a difference between AST borrowck
+// and MIR borrowck that is "fixed by NLL". This test encodes
+// that NLL mode and Polonius mode make this compile-fail, but
+// also that this NLL error is turned into a warning in the default
+// NLL-migrate mode.
+// The polonius compare-mode runs with the full NLL mode, not the 
+// NLL-migrate mode. We cannot currently encode a test that is
+// run-pass or compile-fail based on compare-mode and nll-mode.
+// So just ignore it instead:
+
+// ignore-compare-mode-polonius
+
 // compile-pass
 #![allow(unused)]
 
